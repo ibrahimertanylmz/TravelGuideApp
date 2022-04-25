6c038a2f1117b2c66025e4b6419e9f9ab1b9bb12
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-25 17:08:01 +0300

    getPlaces database operation added

--- app/src/main/java/com/turkcell/travelguideapp/dal/TravelGuideOperation.kt
+++ app/src/main/java/com/turkcell/travelguideapp/dal/TravelGuideOperation.kt
@@ -1,7 +1,12 @@
 package com.turkcell.travelguideapp.dal
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import com.google.android.gms.maps.model.LatLng
+import com.turkcell.travelguideapp.model.Place
+import com.turkcell.travelguideapp.model.Priority
 
 class TravelGuideOperation(context: Context) {
     private var travelGuideDatabase: SQLiteDatabase? = null
@@ -20,4 +25,38 @@ class TravelGuideOperation(context: Context) {
             travelGuideDatabase!!.close()
         }
     }
+
+    @SuppressLint("Range")
+    fun getAllPlaces(): ArrayList<Place>{
+        val placeList = ArrayList<Place>()
+        var place : Place
+        open()
+        var cursor : Cursor = getPlaces()
+        if(cursor.moveToFirst()){
+            do {
+                val name = cursor.getString(cursor.getColumnIndex("Name"))
+                val latitude = cursor.getString(cursor.getColumnIndex("Latitude"))
+                val longitude = cursor.getString(cursor.getColumnIndex("Longitude"))
+                val description = cursor.getString(cursor.getColumnIndex("Description"))
+                val definition = cursor.getString(cursor.getColumnIndex("Definition"))
+                val priority = Priority.valueOf(cursor.getString(cursor.getColumnIndex("Priority")))
+
+                place = Place(name, LatLng(latitude.toDouble(),longitude.toDouble()),definition,description,priority)
+                place.id = cursor.getInt(0)
+                placeList.add(place)
+
+            }while (cursor.moveToNext())
+
+        }
+
+        close()
+        return placeList
+    }
+
+
+    private fun getPlaces() : Cursor {
+        val sorgu = "Select * from Place"
+        return travelGuideDatabase!!.rawQuery(sorgu, null)
+    }
+
 }
