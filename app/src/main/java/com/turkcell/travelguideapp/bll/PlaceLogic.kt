321edf733c2a6d6e90c2a86e57a483ed71b90a65
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-25 17:34:24 +0300

    db operations are added to PlaceLogic

--- app/src/main/java/com/turkcell/travelguideapp/bll/PlaceLogic.kt
+++ app/src/main/java/com/turkcell/travelguideapp/bll/PlaceLogic.kt
@@ -1,7 +1,18 @@
 package com.turkcell.travelguideapp.bll
 
+import android.content.Context
+import com.turkcell.travelguideapp.dal.TravelGuideOperation
 import com.turkcell.travelguideapp.model.Place
+import com.turkcell.travelguideapp.model.Visitation
 
 object PlaceLogic {
     val listPlaces = ArrayList<Place>()
+
+    fun getPlaceList(context: Context): ArrayList<Place>{
+        return TravelGuideOperation(context).getAllPlaces()
+    }
+
+    fun getVisitationsOfPlace(placeId: Int, context: Context): ArrayList<Visitation>{
+        return TravelGuideOperation(context).getVisitationsByPlaceId(placeId)
+    }
 }
