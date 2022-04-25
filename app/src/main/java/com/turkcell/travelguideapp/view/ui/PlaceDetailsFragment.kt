fde9038bba75685c88149e2c945ee62d2a2ebc1f
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-04-25 14:36:40 +0300

    added navigation actions to PlaceDetailsFragment.kt

--- app/src/main/java/com/turkcell/travelguideapp/view/ui/PlaceDetailsFragment.kt
+++ app/src/main/java/com/turkcell/travelguideapp/view/ui/PlaceDetailsFragment.kt
@@ -9,12 +9,13 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.turkcell.travelguideapp.BLL.VisitationLogic.visitationList
+import com.turkcell.travelguideapp.BLL.VisitationLogic.listVisitation
 import com.turkcell.travelguideapp.R
 import com.turkcell.travelguideapp.databinding.FragmentPlaceDetailsBinding
 
 class PlaceDetailsFragment : Fragment() {
     private lateinit var binding: FragmentPlaceDetailsBinding
+    var placeId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,9 +32,13 @@ class PlaceDetailsFragment : Fragment() {
         val liste= arrayListOf<String>("Eleman1","Eleman2","Eleman3","Eleman4","Eleman5","Eleman6","Eleman1","Eleman2","Eleman3","Eleman4","Eleman5","Eleman6","Eleman1","Eleman2","Eleman3","Eleman4","Eleman5","Eleman6")
 
         //Adapter= liste yapısının içeriğinin doldurulması ve yönetilmesi işlemini üstlenir.
-        var adap= ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item,liste)
+        val adap= ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item,liste)
         binding.spinner.adapter=adap
         adap.notifyDataSetChanged()// işlemler yapıldıktan sonra kullanılır, adapter'in yenilenmesini sağlar böylece yeni elemanlar gözükür
+
+        //get place id from navigation
+        placeId = requireArguments().getInt("placeId")
+
 
         setupRvVisitHistory()
         initializeEvents()
@@ -43,21 +48,20 @@ class PlaceDetailsFragment : Fragment() {
 
     private fun initializeEvents() {
         binding.btnAddVisitation.setOnClickListener {
-            val action = PlaceDetailsFragmentDirections.actionPlaceDetailsFragmentToAddVisitationFragment()
+            val action = PlaceDetailsFragmentDirections.actionPlaceDetailsFragmentToAddVisitationFragment(placeId)
             requireView().findNavController().navigate(action)
         }
 
         binding.btnShowLocation.setOnClickListener {
             val intent = Intent(requireActivity(), MapsActivity::class.java)
-            //buradaki id nereden gelecek?
-            intent.putExtra("placeId", -1)
+            intent.putExtra("placeId", placeId)
             startActivity(intent)
         }
     }
 
     private fun setupRvVisitHistory() {
         binding.rvVisitHistory.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false)
-        binding.rvVisitHistory.adapter = VisitationAdapter(requireActivity(), visitationList)
+        binding.rvVisitHistory.adapter = VisitationAdapter(requireActivity(), listVisitation)
     }
 
 }
