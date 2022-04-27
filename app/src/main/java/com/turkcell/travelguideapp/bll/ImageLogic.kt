22b230acbffb0aa7189c0996e470148135355e38
ibrahimertanylmz <ibrahimertanylmz@gmail.com>
2022-04-27 03:53:42 +0300

    imageLogic updated

--- app/src/main/java/com/turkcell/travelguideapp/bll/ImageLogic.kt
+++ app/src/main/java/com/turkcell/travelguideapp/bll/ImageLogic.kt
@@ -1,4 +1,17 @@
 package com.turkcell.travelguideapp.bll
 
+import android.content.Context
+import android.graphics.Bitmap
+import com.turkcell.travelguideapp.dal.TravelGuideOperation
+
 object ImageLogic {
+
+    fun getImagesByPlaceId(context: Context, placeId: Int): ArrayList<Bitmap>{
+        return TravelGuideOperation(context).getImagesByPlaceId(placeId)
+    }
+
+    fun addImage(context: Context, bitmap: Bitmap, placeId: Int){
+        TravelGuideOperation(context).addImage(bitmap,placeId)
+    }
+
 }
